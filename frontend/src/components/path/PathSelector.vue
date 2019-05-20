2c1c5b4a4d1610fee4c57f363349af114fdd6e90
tomislav <danijel.martinek.4@gmail.com>
2019-05-21 00:16:28 +0200

    path list connected with step selector, added some styling

---
+++ frontend/src/components/path/PathSelector.vue
@@ -0,0 +1,152 @@
+<template>
+    <div id="selectedPath">
+       <div id="toggleCategoryContainer">
+        <div id="toggleCategory">
+            <label class="select">
+                <input type="radio" checked="checked" @click="changeStepTo(1)" name="radio">
+                <span class="checkmark"></span>
+                <div class="text">{{ path.highSchool }}</div>
+            </label>
+            <div class="line"></div>
+            <label class="select">
+                <input type="radio" @click="changeStepTo(2)" name="radio">
+                <span class="checkmark"></span>
+                <div class="text">{{ path.college }}</div>
+            </label>
+            <div class="line"></div>
+            <label class="select">
+                <input type="radio" @click="changeStepTo(3)" name="radio">
+                <span class="checkmark"></span>
+                <div class="text">{{ path.profession }}</div>
+            </label>
+        </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'toggleStep',
+  props: ['path'],
+	data() {
+		return {
+			step: 1
+		}
+  },
+  mounted(){
+    this.$emit('stepClicked', 1)
+  },
+	methods: {
+		changeStepTo: function(s) {
+      if(this.step != s){
+        this.step = s
+        this.$emit('stepClicked', s)
+      }
+    }
+	}
+}
+</script>
+
+<style>
+#selectedPath{
+    width: 100%;
+    height: 100%;
+
+    background-color: green;
+}
+
+@media only screen and (min-width: 1070px) {
+
+    #selectedPath{
+        position: fixed;
+        width: 80%;
+        margin-left: 20%;
+    }
+}
+
+
+#toggleCategoryContainer{
+  position: fixed;
+  height: 20%;
+  width: 80%;
+  right: 0;
+  padding-top: 2em;
+  
+  background-color: #0a2763;
+}
+
+#toggleCategory{
+  width: 80%;
+  position: relative;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  padding: 1em;
+}
+
+#toggleCategory:checked{
+  right: 0%;
+}
+
+
+.select {
+  width: 16%;
+  float: left;
+  position: relative;
+  margin: 0.5em 0.15em 1.5em 0.15em;
+  cursor: pointer;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+}
+
+.text {
+  position: static;
+  padding-top: 1em;
+  font-size: 1em;
+  font-weight: bold;
+  font-family: Roboto;
+  color: white;
+  text-transform: uppercase;
+  text-align: center;
+}
+
+/* Hide the browser's default radio button */
+.select input {
+  position: absolute;
+  opacity: 0;
+  cursor: pointer;
+}
+
+/* Create a custom radio button */
+.checkmark {
+  position: static;
+  display: block;
+  margin: 0 auto;
+  height: 20px;
+  width: 20px;
+  background-color: #fff;
+  border-radius: 50%;
+}
+
+/* On mouse-over, add a grey background color */
+.select:hover input ~ .checkmark {
+  background-color: #ffc9c2;
+}
+
+/* When the radio button is checked, add a blue background */
+.select input:checked ~ .checkmark {
+  background-color: #f53b24;
+  box-shadow: 1px 1px 7px #f53b24;
+}
+
+.line{
+  width: 24%;
+  height: 5px;
+  float: left;
+  margin: 1em 0em 0em 0em;
+  background-color: #fff;
+  border-radius: 25px;
+}
+</style>
--- frontend/src/components/path/index.vue
+++ frontend/src/components/path/index.vue
@@ -4,29 +4,40 @@
         <div id="pathSelectContainer">
             <div class="heading">Odaberi fakultet i zanimanje</div>
             <div id="pathSelect">
-                <div v-for="(path, i) in paths" :key="i">
-                    {{path.college}}
-                    <br>
-                    {{path.profession}}
+                <div class="pathListElement pathSpacer" v-for="(path, i) in paths" :key="i" @click="changeSelectedPath(i)">
+                    <div>
+                        {{path.college}}
+                        <br>
+                        {{path.profession}}
+                    </div>
                 </div>
+                <div class="dummyPath"></div>
             </div>
         </div>
 
-        <selectedPath></selectedPath>
+        <pathSelector :path="selectedPath" @stepClicked="changeSelectedStep"></pathSelector>
 
 	</div>   
 </template>
 
 <script>
-import selectedPath from "@/components/path/SelectedPath.vue"
+import pathSelector from "@/components/path/PathSelector.vue"
 
 export default {
 	name: 'myPath',
 	components: {
-		selectedPath
+		pathSelector
 	},
 	data() {
 		return {
+            currentSelectedPathIndex: 1,
+            selectedPath: {
+                highSchool: "Gimnazija",
+                college: "TVZ",
+                profession: "Programmer"
+            },
+            selectedStep: {},
+            selectedStepNumber: 1,
 			paths: [
                 {
                     highSchool: "Gimnazija IZD",
@@ -44,7 +55,7 @@ export default {
                     profession: "Inžinjer aeronautike"
                 },
                 {
-                    highSchool: "Gimnazija IZD",
+                    highSchool: " IZD",
                     college: "TVZ",
                     profession: "Programmer"
                 },
@@ -90,10 +101,46 @@ export default {
                 }
             ]
 		}
-	},
+    },
+    mounted() {
+        this.selectPathStyle(0)
+    },
 	methods: {
-		
-	}
+        changeSelectedPath: function(index) {
+            this.selectedPath = this.paths[index]
+
+            if(this.selectedStepNumber == 1){
+                this.selectedStep = this.selectedPath.highSchool
+            } else if(this.selectedStepNumber == 2) {
+                this.selectedStep = this.selectedPath.college
+            } else if(this.selectedStepNumber == 3) {
+                this.selectedStep = this.selectedPath.profession
+            }
+
+            this.selectPathStyle(index)
+        },
+        changeSelectedStep: function(s) {
+            this.selectedStepNumber = s
+            if(s == 1){
+                this.selectedStep = this.selectedPath.highSchool
+            } else if(s == 2) {
+                this.selectedStep = this.selectedPath.college
+            } else if(s == 3) {
+                this.selectedStep = this.selectedPath.profession
+            }
+
+            console.log(this.selectedStep)
+        },
+
+        selectPathStyle(index){
+			let paths = document.getElementsByClassName("pathListElement")
+			
+			paths[this.currentSelectedPathIndex].classList.remove("pathSelected")
+			this.currentSelectedPathIndex = index
+			
+			paths[index].classList.add("pathSelected")
+		}
+    }
 }
 </script>
 
@@ -102,7 +149,7 @@ export default {
     width: 100%;
     padding: 0.2em 0em 0.2em 0em;
 
-    background-color: red;
+    background-color: #0a2155;
 }
 
 #pathSelectContainer .heading{
@@ -111,10 +158,11 @@ export default {
     font-size: 1em;
     font-weight: bold;
     text-transform: uppercase;
-    color: #ffffff;
+    color: #fff;
 }
 
 #pathSelect{
+    display: inline-block;
     width: 100%;
     height: 20vh;
     padding: 0.2em 0em 0.2em 0em;
@@ -122,7 +170,7 @@ export default {
     overflow-x: scroll;
     white-space: nowrap;
 
-    background-color: red;
+    background-color: #0a2155;
 }
 
 /* width */
@@ -132,26 +180,55 @@ export default {
 
 /* Track */
 #pathSelect::-webkit-scrollbar-track {
-  background: gray; 
+  background: #001a4d; 
 }
  
 /* Handle */
 #pathSelect::-webkit-scrollbar-thumb {
-  background: black; 
+  background: #2d3953; 
 }
 
 /* Handle on hover */
 #pathSelect::-webkit-scrollbar-thumb:hover {
-  background: #555; 
+  background: #0a2155; 
 }
 
-#pathSelect > div{
+.pathListElement{
     display: inline-block;
-    width: 50%;
-    margin: 0.5em;
+    margin: 1em 0.5em 1em 0.5em;
     padding: 1em;
+}
 
-    background-color: blue;
+.pathListElement > div{
+    display: inline-block;
+    width: 100%;
+    height: 100%;
+    padding: 1.5em;
+
+    color: #fff;
+    font-family: Roboto;
+    
+
+    background-color: #0c2869;
+    border-radius: 10px;
+
+    cursor: pointer;
+}
+
+#pathSelect > .pathSpacer{
+    border-radius: 15px;
+    padding: 0.2em;
+}
+
+#pathSelect > .pathSelected{
+    background: #f54925;
+    border-radius: 15px;
+    padding: 0.2em;
+}
+
+.dummyPath{
+    height: 20%;
+    background-color: #0a2155 !important;
 }
 
 @media only screen and (min-width: 1070px) {
@@ -175,11 +252,24 @@ export default {
         width: 8px;
     }
 
-    #pathSelect > div{
+    .pathListElement{
         display: block;
-        width: 90%;
-        height: 100px;
-        margin: 1em;
+        width: 95%;
+    }
+
+    .pathListElement > div {
+        width: 100%;
+        height: 100%;
+        padding: 1.5em;
+
+        color: #fff;
+        font-family: Roboto;
+        
+
+        background-color: #0c2869;
+        border-radius: 10px;
+
+        cursor: pointer;
     }
 }
 </style>
