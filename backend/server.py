9558dd1ba6fbb2cce389362a01666885e1583722
Wulvi <40912247+Wulvi@users.noreply.github.com>
2019-05-23 14:42:46 +0200

    updated server, added some routes

--- backend/server.py
+++ backend/server.py
@@ -1,23 +1,35 @@
-from flask import Flask
+from flask import Flask, request, Response
 from flask_pymongo import PyMongo
 import json
 from bson import ObjectId
 from api.mainModule import mainAlgorithm
 from api.secondModule import secondAlgorithm
 from api.thirdModule import thirdAlgorithm
+import datetime
+from flask_cors import CORS
 
 class JSONEncoder(json.JSONEncoder):
     def default(self, o):
         if isinstance(o, ObjectId):
             return str(o)
-        return json.JSONEncoder.default(self, o)
+        elif isinstance(o, datetime.datetime):
+            return o.isoformat()
+        elif isinstance(o, datetime.date):
+            return o.isoformat()
+        elif isinstance(o, datetime.timedelta):
+            return (datetime.datetime.min + o).time().isoformat()
+        else:
+            return json.JSONEncoder.default(self, o)
+    def response(self, obj):
+        return Response(self.encode(obj), mimetype='application/json')
 
 
 app = Flask(__name__)
 
+CORS(app, resources={r"/api/*": {"origins": "*"}})
+
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mc2"
 mongo = PyMongo(app)
-
   
 def konverzija(kolekcija):
     lista = []
@@ -39,7 +51,37 @@ p1 = mainAlgorithm(skole, fakulteti, zanimanja, osoba1)
 p2 = secondAlgorithm(skole, fakulteti, zanimanja, osoba1)
 p3 = thirdAlgorithm(skole, fakulteti, zanimanja, osoba1)
 
-@app.route('/')
-def hello():
-    return JSONEncoder().encode(p3.izlaz())
+@app.route('/fakulteti')
+def dohvatiFakultete():
+    fakulteti = JSONEncoder().response(fakulteti)
+    return fakulteti
+
+@app.route('/skole')
+def dohvatiSkole():
+    skole = JSONEncoder().response(skole)
+    return skole
+
+@app.route('/zanimanja')
+def dohvatiZanimanja():
+    zanimanja = JSONEncoder().response(zanimanja)
+    return zanimanja
+
+@app.route('/getstepdata', methods = ['POST'])
+def sendStepData():
+    req = request.get_json(force=True)
+    skolaID = req['highSchool']
+    fakultetID = req['college']
+    zanimanjeID = req['profession']
+    
+    skola = mongo.db.skole.find_one({"_id" : ObjectId(skolaID)})
+    fakultet = mongo.db.fakulteti.find_one({"_id" : ObjectId(fakultetID)})
+    zanimanje = mongo.db.zanimanja.find_one({"_id" : ObjectId(zanimanjeID)})
+
+    res = {
+        "highSchool" : skola,
+        "college" : fakultet,
+        "profession" : zanimanje
+    }
+
+    return JSONEncoder().response(res)
     
